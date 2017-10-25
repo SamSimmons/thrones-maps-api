29fcb3bc824ce8d64311e4dc3bace8847624deb5
sam simmons <samuel.j.simmons@gmail.com>
2017-10-25 18:28:37 +1300

    add server file structure, add hello-world route

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa')
+const cors = require('kcors')
+const log = require('./logger')
+const api = require('./api')
+
+const app = new Koa()
+const port = process.env.PORT || 5000
+
+const origin = process.env.CORS_ORIGIN | '*'
+app.use(cors({ origin }))
+
+// Log all requests
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const responseTime = Date.now() - start
+  log.info(`${ctx.method} ${ctx.status} ${ctx.url} - ${responseTime} ms`)
+})
+
+// Catch all uncaught errors as they bubble up
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.status = err.status || 500
+    ctx.body = err.message
+    log.error(`Request Error ${ctx.url} - ${err.message}`)
+  }
+})
+
+app.use(api.routes(), api.allowedMethods())
+
+app.listen(port, () => { log.info(`Server listening at port ${port}`) })
