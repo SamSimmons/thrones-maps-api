6f22dd4a1d6b77a557f0c66b075b9920662ebd80
sam simmons <samuel.j.simmons@gmail.com>
2017-10-25 22:53:55 +1300

    bolt on db, add simple endpoints for locations and kingdoms

--- server/api.js
+++ server/api.js
@@ -10,4 +10,34 @@ router.get('/hello', async (ctx) => {
   ctx.body = 'Hello World'
 })
 
+router.get('/time', async (ctx) => {
+  const result = await database.queryTime()
+  ctx.body = result
+})
+
+router.get('/locations/:type', async (ctx) => {
+  const type = ctx.params.type
+  const results = await database.getLocations(type)
+  if (results.length === 0) { ctx.throw(404) }
+
+  const locations = results.map((row) => {
+    let geojson = JSON.parse(row.st_asgeojson)
+    geojson.properties = { name: row.name, type: row.type, id: row.gid }
+    return geojson
+  })
+  ctx.body = locations
+})
+
+router.get('/kingdoms', async (ctx) => {
+  const results = await database.getKingdomBoundaries()
+  if (results === 0) { ctx.throw(404) }
+
+  const boundaries = results.map((row) => {
+    let geojson = JSON.parse(row.st_asgeojson)
+    geojson.properties = { name: row.name, id: row.gid }
+    return geojson
+  })
+  ctx.body = boundaries
+})
+
 module.exports = router
