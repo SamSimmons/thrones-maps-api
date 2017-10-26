e0e47d7dd8f901a7622416a6592c6d97ae9898fb
sam simmons <samuel.j.simmons@gmail.com>
2017-10-26 22:45:03 +1300

    add new routes for kingdoms and locations, add validation to routes with params

--- server/api.js
+++ server/api.js
@@ -1,8 +1,15 @@
 const Router = require('koa-router')
 const database = require('./database')
-const cache = require('./cache')
 const joi = require('joi')
 const validate = require('koa-joi-validate')
+
+const idValidator = validate({
+  params: { id: joi.number().min(0).max(1000).required() }
+})
+
+const typeValidator = validate({
+  params: { type: joi.string().valid(['castle', 'city', 'town', 'ruin', 'landmark', 'region']).required() }
+})
 
 const router = new Router()
 
@@ -15,7 +22,7 @@ router.get('/time', async (ctx) => {
   ctx.body = result
 })
 
-router.get('/locations/:type', async (ctx) => {
+router.get('/locations/:type', typeValidator, async (ctx) => {
   const type = ctx.params.type
   const results = await database.getLocations(type)
   if (results.length === 0) { ctx.throw(404) }
@@ -28,7 +35,35 @@ router.get('/locations/:type', async (ctx) => {
   ctx.body = locations
 })
 
+router.get('/kingdoms/:id/size', idValidator, async (ctx) => {
+  const id = ctx.params.id
+  const result = await database.getRegionSize(id)
+  if (!result) { ctx.throw(404) }
+
+  const sqKm = result.size * (10 ** -6)
+  ctx.body = sqKm
+})
+
+router.get('/kingdoms/:id/castles', idValidator, async (ctx) => {
+  const regionId = ctx.params.idea
+  const result = await database.countCastles(regionId)
+  ctx.body = result ? result.count : ctx.throw(404)
+})
+
+router.get('/kingdoms/:id/summary', idValidator, async (ctx) => {
+  const id = ctx.params.id
+  const result = await database.getSummary('kingdoms', id)
+  ctx.body = result || ctx.throw(404)
+})
+
+router.get('/locations/:id/summary', idValidator, async (ctx) => {
+  const id = ctx.params.id
+  const result = await database.getSummary('locations', id)
+  ctx.body = result || ctx.throw(404)
+})
+
 router.get('/kingdoms', async (ctx) => {
+  console.log("Defo comming through")
   const results = await database.getKingdomBoundaries()
   if (results === 0) { ctx.throw(404) }
 
